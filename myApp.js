4de0e58567e895032b3640056511c8a3cf9dc4f8
miafate <miapurplefroot@gmail.com>
2024-01-12 13:35:20 -0300

    added static files

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
-
+//__dirname/public is mounted in /public dir of our website
+app.use("/public", express.static(__dirname + "/public"))
 //const handler = (req, res) => {
 //  res.send("Hello Express")
 //}
