63a1032c71be10b0c2baf7cd8a07d9df21e69d2e
miafate <miapurplefroot@gmail.com>
2024-01-12 14:36:15 -0300

    chaining middlewares in GET method with handler

--- myApp.js
+++ myApp.js
@@ -6,19 +6,34 @@ app.use("/public", express.static(__dirname + "/public"))
 //const handler = (req, res) => {
 //  res.send("Hello Express")
 //}
+
+//middleware for all routes
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next()
-})
+});
+//sendfile in the route response
 const absolutePath = __dirname + '/views/index.html'
 app.get("/", (req, res) => {
   res.sendFile(absolutePath)
-})
+});
+
+//sending jason
 app.get("/json", (req, res) => {
   let message = "Hello json"
 
   res.json({ "message": `${process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message}` })
+});
+
+//chaining middleware in /now route
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({ "time": `${req.time}` })
 })
+
+
 
 
 
